f1c69c2455c27ea270d3d8561f7ebd81811b34ab
Jimmy Mead <jdmead@asu.edu>
2019-08-16 12:24:32 -0700

    first commit: Validations added and functioning per assignement specs

--- Controllers/HelloController.cs
+++ Controllers/HelloController.cs
@@ -7,25 +7,28 @@ using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
-namespace DojoSurvey.Controllers
-{
-    public class HelloController : Controller
-    {
+namespace DojoSurvey.Controllers {
+    public class HelloController: Controller {
         // GET: /<controller>/
         [HttpGet("")]
-        public IActionResult Index()
-        {
+        public IActionResult Index() {
             return View();
         }
 
         [HttpPost("result")]
-        public IActionResult Result(Survey survey, string test)
-        {
-            ViewBag.Name = survey.Name;
-            ViewBag.Location = survey.Location;
-            ViewBag.FavLang = survey.FavLanguage;
-            ViewBag.Comment = survey.Comment;
-            return View();
+        public IActionResult Result(Survey survey) {
+            if (ModelState.IsValid) {
+                ViewBag.Name = survey.Name;
+                ViewBag.Location = survey.Location;
+                ViewBag.FavLang = survey.FavLanguage;
+                ViewBag.Comment = survey.Comment;
+                return View("result");
+            } else {
+                // Oh no!  We need to return a ViewResponse to preserve the ModelState, and the errors it now contains!
+                return View("Index");
+            }
         }
     }
 }
+
+// 
--- Models/Class.cs
+++ Models/Class.cs
@@ -2,14 +2,21 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
 
 namespace DojoSurvey.Models
 {
     public class Survey
     {
+        [Required]
+        [MinLength(2)]
         public string Name { get; set; }
+        [Required]
         public string Location { get; set; }
+        [Required]
         public string FavLanguage { get; set; }
+        [MaxLength(20)]
         public string Comment { get; set; } 
     }
 }
